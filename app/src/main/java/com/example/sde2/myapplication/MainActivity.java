f1c1f3b197958e4caf31b4eee39c0f1b02176074
e1211303 <e1211303@niihama.kosen-ac.jp>
2018-10-20 07:58:00 +0900

    お部屋からのコミット

--- app/src/main/java/com/example/sde2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sde2/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,13 +19,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ((TextView)findViewById(R.id.textView_welcome)).setText(
                 Html.fromHtml(getString(R.string.Welcome_message))
         );
+
+        //OnClickListener(このアクティビティ)をセット
+        ((Button)findViewById(R.id.button_begin)).setOnClickListener(this);
+        ((Button)findViewById(R.id.button_continue)).setOnClickListener(this);
     }
 
     //何かクリックされた
     public void onClick(View view){
-        switch (view.getId()){
+        int id = view.getId();
+        switch (id){
             case R.id.button_begin: //始めるボタン
-                Intent intent = new Intent(getApplication(),RowAndColumnActivity.class);
+                Intent intent =new Intent(getApplication(),RowAndColumnActivity.class);
                 startActivity(intent);
                 break;
         }
